4b6d87cfe1a3300b056ca7becee82c4f038cc0d6
markthree <1801982702@qq.com>
2025-06-07 16:51:33 +0800

    fix: 修复缓存

--- middlewares/cache.ts
+++ middlewares/cache.ts
@@ -17,7 +17,7 @@ const defaultOptions: Options = {
   // 默认 1 年的缓存时间
   cacheControl: "public, max-age=31536000, immutable",
   ignore(ctx) {
-    const base = basename(ctx.req.url);
+    const base = basename(ctx.req.path);
 
     // 如果是 HTML 页面，则不缓存
     const isHtml = base.endsWith(".html");
