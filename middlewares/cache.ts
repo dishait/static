84e0a4597a031a8e906d69b5606dc43fe6291b72
markthree <1801982702@qq.com>
2025-06-07 16:33:00 +0800

    fix: 修复缓存

--- middlewares/cache.ts
+++ middlewares/cache.ts
@@ -1,3 +1,4 @@
+import { basename } from "https://deno.land/std@0.196.0/path/mod.ts";
 import { Context } from "https://deno.land/x/hono@v4.0.7/context.ts";
 import { STATUS_CODE } from "https://deno.land/std@0.217.0/http/status.ts";
 import type { MiddlewareHandler } from "https://deno.land/x/hono@v4.0.7/types.ts";
@@ -16,12 +17,12 @@ const defaultOptions: Options = {
   // 默认 1 年的缓存时间
   cacheControl: "public, max-age=31536000, immutable",
   ignore(ctx) {
-    const url = ctx.req.url;
+    const base = basename(ctx.req.url);
 
     // 如果是 HTML 页面，则不缓存
-    const isHtml = url.endsWith(".html");
+    const isHtml = base.endsWith(".html");
     // 如果不是带 hash 的静态资源，则不缓存
-    const hasHash = hashReg.test(url);
+    const hasHash = hashReg.test(base);
 
     return isHtml || !hasHash;
   },
